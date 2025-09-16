79cee53b5f72b98f401904bc257be285e8f8bc73
Ajay Chouhan <ajay.chouhan@yunicorn.in>
2025-09-16 14:39:33 +0530

    Navigation between all Screen

--- lib/views/screens/login_screen.dart
+++ lib/views/screens/login_screen.dart
@@ -25,7 +25,7 @@ class _LoginScreenState extends State<LoginScreen> {
         if (emailController.text == "test@test.com" &&
             passwordController.text == "11111") {
           ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text("Login Success ✅")),
+            const SnackBar(content: Text("Login Success ✅ ----- by Ajay chouhan")),
           );
         } else {
           ScaffoldMessenger.of(context).showSnackBar(
