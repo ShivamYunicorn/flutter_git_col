bcef8927b37630664a5e87509f46bfc7d478cd1f
Shivam Patel <shivam.patel@yunicorn.in>
2025-09-24 15:19:48 +0530

    dummy test 2

--- lib/views/screens/HomeScreen.dart
+++ lib/views/screens/HomeScreen.dart
@@ -7,7 +7,7 @@ class Homescreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
        appBar: AppBar(
-         title: Text('Home Screen'),
+         title: Text('Home Screen 2'),
 
        ),
 
